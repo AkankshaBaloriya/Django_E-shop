2ec1ff6a4967126929ca7061648c5e39b485c630
AkankshaBaloriya <baloriyaakanksha@gmail.com>
2023-05-20 13:06:38 +0530

    test to deploy

--- Eshop/urls.py
+++ Eshop/urls.py
@@ -3,7 +3,12 @@ from django.contrib import admin
 from django.urls import path,include
 from . import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("store.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns+=staticfiles_urlpatterns()
